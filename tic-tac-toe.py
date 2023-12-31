3d36de012c05bd819f10460b7b336c84a801d427
Kristen Cho <kristencho119@gmail.com>
2023-12-31 01:10:05 -0500

    end

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,25 +10,25 @@
 # 3    |   |
 
 row = ["   ", "|", "   ", "|", "   "]
-sep = "-----------"
+sep = " -----------"
 x = " x "
 o = " o "
 rows = ''.join(row)
-print(" "+rows,"\n",sep,"\n",rows,"\n",sep, "\n",rows)
+print("   a   b   c", "\n", "1"+rows, "\n", sep, "\n", "2"+rows, "\n", sep, "\n", "3"+rows)
 spaces = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 open_spaces = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 
+
 # choosing empty square
 def choose_box():
-    uinput = input("choose spot: ").lower()
+    uinput = input('Choose a spot- a, b, or c for the column, and 1, 2, or 3 for the row (e.g., a1 for the top left corner, b3 for the bottom middle square) or type "end" to end the game.: ').lower()
     if uinput in open_spaces:
-        print(uinput)
         open_spaces.remove(uinput)
-        print(open_spaces, spaces)
+        return uinput
+    elif uinput == "end":
         return uinput
     else: 
         return choose_box()
 
-e = choose_box()
-
-print(e)
+list_rows = [["   ", "|", "   ", "|", "   "], ["   ", "|", "   ", "|", "   "], ["   ", "|", "   ", "|", "   "]]
+def place_markers(spot):
