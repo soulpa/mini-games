913b709f1e3277a4cd329b15a900c051d07e5d54
Kristen Cho <kristencho119@gmail.com>
2023-12-30 23:17:47 -0500

    choose box returns none when recurs for soem reason

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -15,3 +15,18 @@ x = " x "
 o = " o "
 rows = ''.join(row)
 print(" "+rows,"\n",sep,"\n",rows,"\n",sep, "\n",rows)
+spaces = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+open_spaces = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+
+# choosing empty square
+def choose_box():
+    uinput = input("choose spot: ").lower()
+    if uinput in open_spaces:
+        print(uinput)
+        open_spaces.remove(uinput)
+        print(open_spaces, spaces)
+        return uinput
+    else: choose_box()
+
+e = choose_box()
+print(e)
