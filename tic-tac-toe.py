2e0a2b7b8eb2e32cc6f916776ea4e008f01df836
Kristen Cho <kristencho119@gmail.com>
2024-01-06 11:06:14 -0500

    prints board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -14,13 +14,14 @@ sep = " -----------"
 x = " x "
 o = " o "
 rows = ''.join(row)
-print("   a   b   c", "\n", "1"+rows, "\n", sep, "\n", "2"+rows, "\n", sep, "\n", "3"+rows)
+# print("   a   b   c", "\n", "1"+rows, "\n", sep, "\n", "2"+rows, "\n", sep, "\n", "3"+rows)
 spaces = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 open_spaces = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 
 
 # choosing empty square
 def choose_box():
+    global open_spaces
     uinput = input('Choose a spot- a, b, or c for the column, and 1, 2, or 3 for the row (e.g., a1 for the top left corner, b3 for the bottom middle square) or type "end" to end the game.: ').lower()
     if uinput in open_spaces:
         open_spaces.remove(uinput)
@@ -31,4 +32,16 @@ def choose_box():
         return choose_box()
 
 list_rows = [["   ", "|", "   ", "|", "   "], ["   ", "|", "   ", "|", "   "], ["   ", "|", "   ", "|", "   "]]
-def place_markers(spot):
+#def place_markers(spot):
+
+def print_board():
+    print("  a   b   c")
+    first = True
+    for index, element in enumerate(list_rows, start =0):
+        if first:
+            first = False
+        else: 
+            print(sep)
+        print(str(index+1)+''.join(element))
+        
+print_board()
